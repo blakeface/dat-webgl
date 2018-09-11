c7b1d5d2aabd672a22fb3473d8ac8895c0bf763f
netzebandito <blake.netzeband@gmail.com>
2018-09-11 11:15:21 -0600

    idk d3 stuff

--- src/index.js
+++ src/index.js
@@ -3,44 +3,44 @@ import * as d3 from 'd3'
 // style
 import bulma from 'bulma'
 
-const context = document.getElementById('content').getContext('2d');
-
-const width = window.innerWidth;
-const height = window.innerHeight;
-const size = d3.min([width, height]);
-
-d3.select('#content')
-  .attr('width', width + 'px')
-  .attr('height', height + 'px');
-
-context.lineWidth = 0.4;
-context.strokeStyle = 'rgba(0, 0, 0, 0.6)';
-
-const projection = d3.geoOrthographic()
-  .scale(0.45 * size)
-  .translate([0.5 * width, 0.5 * height]);
-
-const geoGenerator = d3.geoPath()
-  .projection(projection)
-  .context(context);
-
-const geojson = {type: 'Feature', geometry: {type: 'LineString', coordinates: []}};
-function rndLon() {return -180 + Math.random() * 360;}
-function rndLat() {return -90 + Math.random() * 180;}
-function addPoint() {geojson.geometry.coordinates.push([rndLon(), rndLat()])}
-
-function update(t) {
-  if (geojson.geometry.coordinates.length < 6000)
-    addPoint();
-
-  projection.rotate([t / 1000]);
-
-  context.clearRect(0, 0, width, height);
-  context.beginPath();
-  geoGenerator(geojson);
-  context.stroke();
-
-  window.requestAnimationFrame(update);
+// Dat protocol
+const datUrl = 'dat://3167d872cec0d2896009a4156409b9e1ce89638d030c42be7c4be02f4e967bf7/'
+try	{
+	const archive = new DatArchive(datUrl)
+}
+catch (e) {
+	console.log('Want to see something cool? Try this out over the dat protocol:')
+	console.log(datUrl)
 }
 
-window.requestAnimationFrame(update);
+const svg = d3.select('svg')
+const svgBox = document.querySelector('svg').getBBox()
+const width = svgBox.width
+const height = svgBox.height
+const angles = d3.range(0, 2 * Math.PI, Math.PI / 200);
+
+const path = svg.append('g')
+    .attr('transform', `translate( ${width / 2}, ${height / 2} )`)
+    .attr('fill', 'none')
+    .attr('stroke-width', 10)
+    .attr('stroke-linejoin', 'round')
+  .selectAll('path')
+  .data(['cyan', 'magenta', 'yellow'])
+  .enter().append('path')
+    .attr('stroke', d => d )
+    .style('mix-blend-mode', 'darken')
+    .datum(function(d, i) {
+      return d3.radialLine()
+          .curve(d3.curveLinearClosed)
+          .angle(function(a) { return a; })
+          .radius(function(a) {
+            const t = d3.now() / 1000;
+            return 200 + Math.cos(a * 8 - i * 2 * Math.PI / 3 + t) * Math.pow((1 + Math.cos(a - t)) / 2, 3) * 32;
+          });
+    });
+
+d3.timer(function() {
+  path.attr('d', function(d) {
+    return d(angles);
+  });
+});
